9715f623691f569e45c43a75a69d2b03b1ecec40
Marvin Engelbrecht <marvinengelbrecht30@gmail.com>
2024-11-08 23:42:43 +0100

    making an environment for colision tests

--- src/App.java
+++ src/App.java
@@ -1,22 +1,113 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import models.*;
+import models.Player;
 
 public class App extends Application {
+
+    // Size of the game window
+    // width 1200, height 900
+    private static final int WINDOW_WIDTH = 720;
+    private static final int WINDOW_HEIGHT = 680;
+    private boolean upPressed = false;
+    private boolean downPressed = false;
+    private boolean leftPressed = false;
+    private boolean rightPressed = false;
+
+    // Player visual representation
+    private Rectangle playerRectangle1;
+    private Rectangle playerRectangle2;
+    private Player player1;
+    private Player player2;
+    private int counter;
+
     @Override
     public void start(Stage primaryStage) {
-        VBox vBox = new VBox(10);
-        Scene scene = new Scene(vBox, 720, 680);
+        player1 = new Player(100, 1, 0, 0);
+        player2 = new Player(100, 3, 50, 50);
 
-        primaryStage.setTitle("Test");
+        playerRectangle1 = new Rectangle(20, 20, Color.BLUE);
+        playerRectangle1.setX(player1.getX());
+        playerRectangle1.setY(player1.getY());
+
+        playerRectangle2 = new Rectangle(20, 20, Color.RED);
+        playerRectangle2.setX(player2.getX());
+        playerRectangle2.setY(player2.getY());
+
+        Pane pane = new Pane();
+        pane.getChildren().addAll(playerRectangle1, playerRectangle2);
+
+        Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
+        primaryStage.setTitle("2D Player Movement");
         primaryStage.setScene(scene);
         primaryStage.show();
-        
+
+        // Event handling for key pressed
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case W -> upPressed = true;
+                case S -> downPressed = true;
+                case A -> leftPressed = true;
+                case D -> rightPressed = true;
+                case ENTER -> primaryStage.close();
+            }
+        });
+
+        // Event handling for key released
+        scene.setOnKeyReleased(event -> {
+            switch (event.getCode()) {
+                case W -> upPressed = false;
+                case S -> downPressed = false;
+                case A -> leftPressed = false;
+                case D -> rightPressed = false;
+            }
+        });
+
+        // Animation timer to update player position continuously
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (!checkColision()) {
+                    updatePlayerPosition();
+                }
+            }
+        };
+        timer.start();
+    }
+
+    private void updatePlayerPosition() {
+        if (upPressed) player1.moveUp();
+        if (downPressed) player1.moveDown();
+        if (leftPressed) player1.moveLeft();
+        if (rightPressed) player1.moveRight();
+
+        // Update the visual representation of the player
+        playerRectangle1.setX(player1.getX());
+        playerRectangle1.setY(player1.getY());
+    }
+
+    private boolean checkColision() {
+        if (playerRectangle1.getX() == playerRectangle2.getX() && playerRectangle1.getY() == playerRectangle2.getY()) {
+            counter++;
+            System.out.println("Colision detected: " + counter);
+            return true;
+        }
+
+        if(Math.abs(playerRectangle1.getX() - playerRectangle2.getX()) <= 10 && Math.abs(playerRectangle1.getY() - playerRectangle2.getY()) <= 10) {
+            counter++;
+            System.out.println("Colision detected: " + counter);
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
--- src/models/Player.java
+++ src/models/Player.java
@@ -5,14 +5,53 @@ public class Player {
     private int health;
     private int maxHealth;
     private int speed;
-
-    public Player(int health, int speed) {
+    private int x;
+    private int y;
+    
+    public Player(int health, int speed, int startX, int startY) {
         this.inventory = new Inventory(50);
         this.health = health;
         this.maxHealth = health;
         this.speed = speed;
+        this.x = startX;
+        this.y = startY;
     }
-
+    
+    // movement methods
+    public void moveUp() {
+        this.y -= this.speed;
+    }
+    
+    public void moveDown() {
+        this.y += this.speed;
+    }
+    
+    public void moveLeft() {
+        this.x -= this.speed;
+    }
+    
+    public void moveRight() {
+        this.x += this.speed;
+    }
+    
+    public boolean addHealth(int amount) {
+        if (amount < 0) {
+            return false;
+        }
+        
+        if (this.health + amount > maxHealth) {
+            this.health = maxHealth;
+        } else {
+            this.health += amount;
+        }
+        return true;
+    } 
+    
+    public void addSpeed(int amount) {
+        this.speed += amount;
+    }
+    
+    // getter and setter methods
     public Inventory getInventory() {
         return this.inventory;
     }
@@ -20,7 +59,7 @@ public class Player {
     public int getHealth() {
         return this.health;
     }
-
+    
     public void setHealth(int health) {
         if (!(health < 0 || health > maxHealth))  {
             this.health = health;
@@ -29,18 +68,6 @@ public class Player {
         }
     }
 
-    public boolean addHealth(int amount) {
-        if (amount < 0) {
-            return false;
-        }
-
-        if (this.health + amount > maxHealth) {
-            this.health = maxHealth;
-        } else {
-            this.health += amount;
-        }
-        return true;
-    } 
 
     public int getSpeed() {
         return this.speed;
@@ -50,7 +77,20 @@ public class Player {
         this.speed = speed;
     }
 
-    public void addSpeed(int amount) {
-        this.speed += amount;
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int newX) {
+        this.x = newX;
+    }
+
+    public void setY(int newY) {
+        this.y = newY;
     }
 }
